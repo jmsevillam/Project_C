753c1d3a4f9c1cd766e3044c952d02611d6e4762
Mauricio Sevilla <jmsevillam@unal.edu.co>
2022-10-06 17:02:48 +0200

    Get coordinates functions done

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
+
+class particle{
+private:
+    double m;
+    double y;
+    double z;
+    double x;
+public:
+    double test;
+    double Get_x(){return x;};
+    double Get_y(){return y;};
+    double Get_z(){return z;};
+};
+
+
+
 int main(int argc, char * argv[]){
 
-std::cout<<"Hello World"<<std::endl;
+particle ball;
+
+std::cout<< ball.Get_x() <<std::endl;
 
 return 0;
 }
